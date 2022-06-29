6ce56218509650b5b863bcdf3845a38059e404c0
WinSpoon <winstone851108@gmail.com>
2022-06-29 16:46:05 +0900

    Framework v6.1 ( CollisionManager )

--- Framework/Framework/CollisionManager.h
+++ Framework/Framework/CollisionManager.h
@@ -16,11 +16,11 @@ public:
 		Vector3 Scale_B = _ObjB->GetScale();
 		
 		// ** 충돌이 되었다면...
-		/*
 		if (Position_A.x + Scale_A.x > Position_B.x &&
 			Position_B.x + Scale_B.x > Position_A.x &&
 			Position_A.y == Position_B.y)
 			return true;
+		/*
 		*/
 
 		// ** 아니라면..
--- Framework/Framework/Player.cpp
+++ Framework/Framework/Player.cpp
@@ -50,8 +50,9 @@ int Player::Update()
 void Player::Render()
 {
 	CursorManager::Draw(
-		TransInfo.Position.x * 0.5f,
-		TransInfo.Position.y * 0.5f,
+		TransInfo.Position.x - (TransInfo.Scale.x * 0.5f),
+		//TransInfo.Position.y - (TransInfo.Scale.y * 0.5f),
+		TransInfo.Position.y,
 		strKey);
 }
 
